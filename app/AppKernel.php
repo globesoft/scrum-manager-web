61f69265508e43b87092f2163c2533961d8a940d
Petre Patrasc <petre@dreamlabs.ro>
2013-10-30 18:22:22 +0200

    Fixes #22: Top bar profile information

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new GlobeSoft\ScrumManagerWebBundle\GSScrumWebBundle(),
             new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
+            new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
